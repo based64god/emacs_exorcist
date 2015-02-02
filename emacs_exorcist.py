7f03fca96349844973c7c55a47b3bd4523c0e8ea
Emmett Hitz <emmetthitz@gmail.com>
2015-02-02 16:57:06 -0500

    Fixed file writing hopefully

--- emacs_exorcist.py
+++ emacs_exorcist.py
@@ -4,6 +4,9 @@ import os
 input_file=raw_input("Please enter the directory of the file you wish to exorcise (or file name if this script is in the same folder). I.E. ~/Desktop/somefolder/somefile.txt:  ")
 print(input_file)
 file_working=open(os.path.expanduser(input_file))
-for line in file_working.readline()
-	line.replace("  ","    ")
-	line.replace("{\n","\n{\n    ")
+output_file=os.path.expanduser(input_file)+"_EXORCISED"
+for line in file_working.read()
+	line.replace("  ","\t")
+	line.replace("{\n","\n{\n\t")
+	output_file.write(line)
+
